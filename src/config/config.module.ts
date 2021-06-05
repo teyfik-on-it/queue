85c1d281c3df4ddd8c7fc91002a493dbfa273f3c
Halil Teyfik <49863255+teyfik-on-it@users.noreply.github.com>
2021-06-05 09:01:36 +0300

    added config module

---
+++ src/config/config.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        PORT: Joi.number().greater(1024).less(65536).required(),
+      }),
+    }),
+  ],
+  exports: [NestConfigModule],
+})
+export class ConfigModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const logger = new Logger('bootstrap');
+const bootstrap = () =>
+  NestFactory.create(AppModule).then((app) => {
+    const configService = app.get(ConfigService);
+    const port = configService.get('PORT');
+
+    return app.listen(port);
+  });
+
+bootstrap().catch(logger.error);
