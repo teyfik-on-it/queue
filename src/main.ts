debf924a4a8409f5d883fab582a5226f35b19e2d
Halil Teyfik <49863255+teyfik-on-it@users.noreply.github.com>
2021-06-05 10:44:39 +0300

    added global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -8,6 +8,13 @@ const bootstrap = () =>
   NestFactory.create(AppModule).then((app) => {
     const configService = app.get(ConfigService);
     const port = configService.get('PORT');
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        forbidUnknownValues: true,
+      }),
+    );
 
     return app.listen(port);
   });
